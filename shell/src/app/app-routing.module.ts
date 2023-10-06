680184c0bb44f018ff5e1b85666cfc1f1f8d6b9d
Toni Škobić <toni.skobic@gmail.com>
2023-10-06 13:59:59 +0200

    shell further setup

--- shell/src/app/app-routing.module.ts
+++ shell/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'demo',
+    loadChildren: () => import('mfe1/Module').then(m => m.DemoModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
